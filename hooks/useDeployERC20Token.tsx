18ead01bbf014ee675724d7899550ce87b1c8f70
Sebastian Guaqueta <scguaquetam@gmail.com>
2024-08-29 13:35:18 -0500

    feat: uploading image to ipfs and passing to contract deployment

--- hooks/useDeployERC20Token.tsx
+++ hooks/useDeployERC20Token.tsx
@@ -3,12 +3,14 @@ import { useCallback, useState } from "react";
 import { ethers } from "ethers";
 import MemeTokenFactoryAbi from "@/contracts/abi/MemeTokenFactory";
 import { DEPLOY_STRATEGY_ENUM } from "@/constants";
+import { UploadImageIpfs } from "@/utils/PinataService";
 
 type DeployERC20Params = {
   name: string;
   symbol: string;
   initialSupply: string;
   maxSupply: string;
+  image: File;
 }
 
 export type DeployERC20Props = DeployERC20Params & {
@@ -27,15 +29,15 @@ const useDeployERC20Token = () => {
 
   const factory = new ethers.Contract(FACTORY_ADDRESS, MemeTokenFactoryAbi, signer);
 
-  const deployInflationaryToken = ({ name, symbol, initialSupply }: DeployERC20Params) => {
+  const deployInflationaryToken = ({ name, symbol, initialSupply }: DeployERC20Params, cid : string) => {
     const _initialSupply = ethers.parseUnits(initialSupply, 18); 
-    return factory.createInflationaryToken(name, symbol, _initialSupply, signerAddress)
+    return factory.createInflationaryToken(name, symbol, _initialSupply, signerAddress, cid)
   }
 
-  const deployDeflationaryToken = ({ name, symbol, initialSupply, maxSupply }: DeployERC20Params) => {
+  const deployDeflationaryToken = ({ name, symbol, initialSupply, maxSupply }: DeployERC20Params, cid: string) => {
     const _initialSupply = ethers.parseUnits(initialSupply, 18); 
     const _maxSupply = ethers.parseUnits(maxSupply, 18);
-    return factory.createDeflationaryToken(name, symbol, _initialSupply, _maxSupply, signerAddress)
+    return factory.createDeflationaryToken(name, symbol, _initialSupply, _maxSupply, signerAddress, cid)
   }
 
   const getContractAddress = async (tx: { hash: any; wait: () => any; }) => {
@@ -59,20 +61,28 @@ const useDeployERC20Token = () => {
     [DEPLOY_STRATEGY_ENUM.INFLATIONARY]: deployInflationaryToken
   }
 
-  const deployERC20 = useCallback(async ({ name, symbol, maxSupply, initialSupply, strategy }: DeployERC20Props) => {
+  const deployERC20 = useCallback(async ({ name, symbol, maxSupply, initialSupply, strategy, image }: DeployERC20Props) => {
     const params: DeployERC20Params = {
       name,
       symbol,
       maxSupply,
-      initialSupply
+      initialSupply,
+      image
     }
     console.log('strategy is', strategy);
+    console.log('image exists?', !!image);
     
-    const tx = await strategyToFunctionMapper[strategy](params)
-    setTxHash(tx.hash)
+    const cid = await UploadImageIpfs(image)
+    if(cid) {
+      const tx = await strategyToFunctionMapper[strategy](params, cid)
+      setTxHash(tx.hash)
+      const contractAddress = await getContractAddress(tx)
+      setContractAddress(contractAddress);
+    } else {
+      console.log('Error uploading image to IPFS');
+      setIsError(true)
+    }
 
-    const contractAddress = await getContractAddress(tx)
-    setContractAddress(contractAddress);
   }, [])
 
   return {
---
+++ utils/PinataService.tsx
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+export async function UploadImageIpfs(image: File) {
+  try {
+    if (!image) {
+      console.log('No image provided')
+      return null
+    }
+    console.log('Uploading image to IPFS...');
+    const data = new FormData()
+    data.append('file', image)
+
+    const response = await axios.post(
+      'https://api.pinata.cloud/pinning/pinFileToIPFS',
+      data,
+      {
+        maxBodyLength: Infinity,
+        headers: {
+          pinata_api_key: process.env.NEXT_PUBLIC_PINATA_API_KEY,
+          pinata_secret_api_key: process.env.NEXT_PUBLIC_PINATA_SECRET_KEY,
+        },
+      }
+    )
+    console.log('IPFS Hash:', response.data.IpfsHash)
+    return response.data.IpfsHash
+  } catch (error) {
+    console.log('Error uploading image is: ', error)
+    return null
+  }
+}
