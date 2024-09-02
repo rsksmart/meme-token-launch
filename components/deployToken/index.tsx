2de79821f2ccbe4bdca11035e9268636c1efcf39
lumaivzqz-rs <lucia.vazquez@rootstocklabs.com>
2024-09-02 14:03:11 +0200

    style: change form style

--- components/deployToken/index.tsx
+++ components/deployToken/index.tsx
@@ -23,7 +23,7 @@ export type DeployFormData = {
 const DeployToken: React.FC = () => {
     const { isLoggedIn } = useAuth();
     const [gasless, setGasless] = useState(true);
-    
+
     const [formData, setFormData] = useState<DeployFormData>({
         name: "",
         symbol: "",
@@ -86,137 +86,172 @@ const DeployToken: React.FC = () => {
                 </CardTitle>
                 <CardDescription>Deploy your meme token on Rootstock!</CardDescription>
             </CardHeader>
-            <CardContent>
-                <div className="">
-                    <div className="flex-row flex gap-2 items-center">
-                        <label htmlFor="strategy" className="block">
-                            Strategy
-                        </label>
-                        <Tooltip>
-                            <TooltipTrigger>
-                                <HelpCircleIcon className="w-4 h-4" />
-                            </TooltipTrigger>
-                            <TooltipContent>
-                                <p>Select the strategy to deploy the ERC20 tokens.</p>
-                            </TooltipContent>
-                        </Tooltip>
-                    </div>
-                    <select
-                        name="strategy"
-                        id="strategy"
-                        value={formData.strategy}
-                        onChange={handleChange}
-                        className="mt-2 w-full px-3 py-2 border border-[hsl(var(--border))] rounded-md bg-[hsl(var(--card))] focus:border-gray-200 focus:outline-none"
-                    >
-                        <option value={DEPLOY_STRATEGY_ENUM.DEFLATIONARY}>{DEPLOY_STRATEGY.DEFLATIONARY.name}</option>
-                        <option value={DEPLOY_STRATEGY_ENUM.INFLATIONARY}>{DEPLOY_STRATEGY.INFLATIONARY.name}</option>
-                    </select>
-
-                </div>
-                <div className="my-4">
-                    <div className="flex-row flex gap-2 items-center">
-                        <label htmlFor="name" className="block">
-                            Name
-                        </label>
-                        <Tooltip>
-                            <TooltipTrigger>
-                                <HelpCircleIcon className="w-4 h-4" />
-                            </TooltipTrigger>
-                            <TooltipContent>
-                                <p>Enter the name of the ERC20 token.</p>
-                            </TooltipContent>
-                        </Tooltip>
-                    </div>
-                    <input
-                        type="text"
-                        name="name"
-                        id="name"
-                        value={formData.name}
-                        onChange={handleChange}
-                        className="mt-2 w-full px-3 py-2 border border-[hsl(var(--border))] rounded-md bg-[hsl(var(--card))] focus:border-gray-200 focus:outline-none"
-                    />
-                </div>
-                <div className="my-4">
-                    <div className="flex-row flex gap-2 items-center">
-                        <label htmlFor="symbol" className="block">
-                            Symbol
-                        </label>
-                        <Tooltip>
-                            <TooltipTrigger>
-                                <HelpCircleIcon className="w-4 h-4" />
-                            </TooltipTrigger>
-                            <TooltipContent>
-                                <p>
-                                    Enter the symbol of the ERC20 token.
-                                </p>
-                            </TooltipContent>
-                        </Tooltip>
-                    </div>
-
-                    <input
-                        type="text"
-                        name="symbol"
-                        id="symbol"
-                        value={formData.symbol}
-                        onChange={handleChange}
-                        className="mt-2 w-full px-3 py-2 border border-[hsl(var(--border))] rounded-md bg-[hsl(var(--card))] focus:border-gray-200 focus:outline-none"
-                    />
-                </div>
-                <div className="mt-4">
-                    <div className="flex-row flex gap-2 items-center">
-                        <label htmlFor="initialSupply" className="">
-                            Initial Supply
-                        </label>
-                        <Tooltip>
-                            <TooltipTrigger>
-                                <HelpCircleIcon className="w-4 h-4" />
-                            </TooltipTrigger>
-                            <TooltipContent>
-                                <p>Enter the Initial Supply for the ERC20 token. It should be in decimal string. </p>
-                            </TooltipContent>
-                        </Tooltip>
-                    </div>
-
-                    <input
-                        type="text"
-                        name="initialSupply"
-                        id="initialSupply"
-                        value={formData.initialSupply}
-                        onChange={handleChange}
-                        className="mt-2 w-full px-3 py-2 border border-[hsl(var(--border))] rounded-md bg-[hsl(var(--card))] focus:border-gray-200 focus:outline-none"
-                    />
-                </div>
-                {formData.strategy == DEPLOY_STRATEGY_ENUM.DEFLATIONARY && (
-                    <div className="mt-4">
+            <CardContent className={!isLoggedIn ? "opacity-40" : ""}>
+                <div className="flex flex-row gap-10 w-full">
+                    <div className="w-full">
                         <div className="flex-row flex gap-2 items-center">
-                            <label htmlFor="maxSupply" className="">
-                                Max Supply
+                            <label htmlFor="strategy" className="block">
+                                Strategy
                             </label>
                             <Tooltip>
                                 <TooltipTrigger>
                                     <HelpCircleIcon className="w-4 h-4" />
                                 </TooltipTrigger>
                                 <TooltipContent>
-                                    <p>Enter the Max Supply for the ERC20 token. It should be in decimal string.</p>
+                                    <p>Select the strategy to deploy the ERC20 tokens.</p>
+                                </TooltipContent>
+                            </Tooltip>
+                        </div>
+                        <select
+                            name="strategy"
+                            id="strategy"
+                            disabled={!isLoggedIn}
+                            value={formData.strategy}
+                            onChange={handleChange}
+                            className="mt-2 w-full px-3 py-2 border border-[hsl(var(--border))] rounded-md bg-[hsl(var(--card))] focus:border-gray-200 focus:outline-none"
+                        >
+                            <option value={DEPLOY_STRATEGY_ENUM.DEFLATIONARY}>{DEPLOY_STRATEGY.DEFLATIONARY.name}</option>
+                            <option value={DEPLOY_STRATEGY_ENUM.INFLATIONARY}>{DEPLOY_STRATEGY.INFLATIONARY.name}</option>
+                        </select>
+
+                    </div>
+                    <div className="w-full">
+                        <div className="flex-row flex gap-2 items-center">
+                            <label htmlFor="gasless">Gasless</label>
+                            <Tooltip>
+                                <TooltipTrigger>
+                                    <HelpCircleIcon className="w-4 h-4" />
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                    <p>Active this option if you don't have enough rBTC.</p>
+                                </TooltipContent>
+                            </Tooltip>
+
+                        </div>
+                        <label className={(!isLoggedIn ? "cursor-default " : "") + "flex relative items-center cursor-pointer mt-2"}>
+                            <input
+                                checked={gasless}
+                                name="gasless"
+                                type="checkbox"
+                                className="sr-only"
+                                disabled={!isLoggedIn}
+                                onChange={(e) => setGasless(Boolean(e.target.checked))}
+                            />
+                            <span className={(!isLoggedIn ? "cursor-default " : "") + "w-11 h-6 bg-card rounded-full border border-input toggle-bg"}></span>
+                        </label>
+                    </div>
+
+                </div>
+                <div className="my-4 flex flex-row gap-10">
+                    <div className="w-full">
+                        <div className="flex-row flex gap-2 items-center">
+                            <label htmlFor="name" className="block">
+                                Name
+                            </label>
+                            <Tooltip>
+                                <TooltipTrigger>
+                                    <HelpCircleIcon className="w-4 h-4" />
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                    <p>Enter the name of the ERC20 token.</p>
+                                </TooltipContent>
+                            </Tooltip>
+                        </div>
+                        <input
+                            type="text"
+                            name="name"
+                            id="name"
+                            disabled={!isLoggedIn}
+                            value={formData.name}
+                            onChange={handleChange}
+                            className="mt-2 w-full px-3 py-2 border border-[hsl(var(--border))] rounded-md bg-[hsl(var(--card))] focus:border-gray-200 focus:outline-none"
+                        />
+                    </div>
+                    <div className="w-full">
+                        <div className="flex-row flex gap-2 items-center">
+                            <label htmlFor="symbol" className="block">
+                                Symbol
+                            </label>
+                            <Tooltip>
+                                <TooltipTrigger>
+                                    <HelpCircleIcon className="w-4 h-4" />
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                    <p>
+                                        Enter the symbol of the ERC20 token.
+                                    </p>
                                 </TooltipContent>
                             </Tooltip>
                         </div>
 
                         <input
                             type="text"
-                            name="maxSupply"
-                            id="maxSupply"
-                            value={formData.maxSupply}
+                            name="symbol"
+                            id="symbol"
+                            disabled={!isLoggedIn}
+                            value={formData.symbol}
                             onChange={handleChange}
                             className="mt-2 w-full px-3 py-2 border border-[hsl(var(--border))] rounded-md bg-[hsl(var(--card))] focus:border-gray-200 focus:outline-none"
                         />
                     </div>
-                )}
+                </div>
+                <div className="w-full mt-4 flex flex-row gap-10">
+                    <div className="w-full">
+                        <div className="flex-row flex gap-2 items-center">
+                            <label htmlFor="initialSupply" className="">
+                                Initial Supply
+                            </label>
+                            <Tooltip>
+                                <TooltipTrigger>
+                                    <HelpCircleIcon className="w-4 h-4" />
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                    <p>Enter the Initial Supply for the ERC20 token. It should be in decimal string. </p>
+                                </TooltipContent>
+                            </Tooltip>
+                        </div>
+
+                        <input
+                            type="text"
+                            name="initialSupply"
+                            id="initialSupply"
+                            disabled={!isLoggedIn}
+                            value={formData.initialSupply}
+                            onChange={handleChange}
+                            className="mt-2 w-full px-3 py-2 border border-[hsl(var(--border))] rounded-md bg-[hsl(var(--card))] focus:border-gray-200 focus:outline-none"
+                        />
+                    </div>
+                    {formData.strategy == DEPLOY_STRATEGY_ENUM.DEFLATIONARY && (
+                        <div className="w-full">
+                            <div className="flex-row flex gap-2 items-center">
+                                <label htmlFor="maxSupply" className="">
+                                    Max Supply
+                                </label>
+                                <Tooltip>
+                                    <TooltipTrigger>
+                                        <HelpCircleIcon className="w-4 h-4" />
+                                    </TooltipTrigger>
+                                    <TooltipContent>
+                                        <p>Enter the Max Supply for the ERC20 token. It should be in decimal string.</p>
+                                    </TooltipContent>
+                                </Tooltip>
+                            </div>
+
+                            <input
+                                type="text"
+                                name="maxSupply"
+                                id="maxSupply"
+                                disabled={!isLoggedIn}
+                                value={formData.maxSupply}
+                                onChange={handleChange}
+                                className="mt-2 w-full px-3 py-2 border border-[hsl(var(--border))] rounded-md bg-[hsl(var(--card))] focus:border-gray-200 focus:outline-none"
+                            />
+                        </div>
+                    )}
+                </div>
                 <div className="mt-4">
-                    <div className="flex-row flex gap-2 items-center">
-                        <label htmlFor="image" className="">
-                            Memetoken logo
-                        </label>
+                    <div className="flex-row flex gap-2 items-center mb-2">
+                        Memetoken logo
                         <Tooltip>
                             <TooltipTrigger>
                                 <HelpCircleIcon className="w-4 h-4" />
@@ -226,40 +261,42 @@ const DeployToken: React.FC = () => {
                             </TooltipContent>
                         </Tooltip>
                     </div>
-                    <input
-                        type="file"
-                        name="image"
-                        id="image"
-                        accept="image/png, image/jpeg"
-                        onChange={handleChange}
-                        className="mt-2 w-full px-3 py-2 border border-[hsl(var(--border))] rounded-md bg-[hsl(var(--card))] focus:border-gray-200 focus:outline-none"
-                    />
+                    <div className="w-full flex flex-col pt-6 pb-4 justify-center items-center gap-6  border border-[hsl(var(--border))] rounded-md">
+                        <div className="h-[200px] w-[200px]">
+                            {formData.image ? (
+                                <img
+                                    src={URL.createObjectURL(formData.image)}
+                                    alt="Selected preview"
+                                    className="w-full h-full rounded-md"
+                                />
+                            ) : (
+                                <div className="w-full h-full bg-gray-300 rounded-full flex items-center justify-center">
+                                    <span className="text-2xl">N/A</span>
+                                </div>
+                            )}
+
+                        </div>
+                        <label
+                            htmlFor="image"
+                            className={"w-[190px] py-2 flex items-center justify-center rounded-full font-bold bg-custom-cyan text-black " + (!isLoggedIn ? "cursor-default" : " cursor-pointer hover:opacity-70 transition")}
+                        >
+                            {formData.image ? formData.image.name : 'Select a file...'}
+                        </label>
+                        <input
+                            type="file"
+                            name="image"
+                            id="image"
+                            disabled={!isLoggedIn}
+                            accept="image/png, image/jpeg"
+                            onChange={handleChange}
+                            className="hidden"
+                        />
+                    </div>
                 </div>
-                
             </CardContent>
-            <CardFooter className="px-0 relative justify-end mb-6 mr-6">
-                <div className="mr-10">
-                    <label htmlFor="">Gasless</label>
-                    <Tooltip>
-                            <TooltipTrigger>
-                                <HelpCircleIcon className="w-4 h-4" />
-                            </TooltipTrigger>
-                            <TooltipContent>
-                                <p>Active this option if you dont have enough rBTC.</p>
-                            </TooltipContent>
-                        </Tooltip>
-                    <label className="flex relative items-center cursor-pointer">
-                    <input
-                        checked={gasless}
-                        type="checkbox"
-                        className="sr-only"
-                        onChange={(e) => setGasless(Boolean(e.target.checked))}
-                    />
-                    <span className="w-11 h-6 bg-card rounded-full border border-input toggle-bg"></span>
-                    </label>
-                </div>
+            <CardFooter className="px-8 relative justify-end mb-6">
                 {isLoggedIn ? (
-                    <DeployERC20TokenButton disabled={!isFormCompleted} params={formData} gasless={gasless}/>
+                    <DeployERC20TokenButton disabled={!isFormCompleted} params={formData} gasless={gasless} />
                 ) : (
 
                     <ConnectWalletButton title="Connect wallet to deploy" />
