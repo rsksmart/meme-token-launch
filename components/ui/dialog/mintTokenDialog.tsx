7134b4ae06fdd299f6ad8e652849f1c5ce11bd46
Sebastian Guaqueta <scguaquetam@gmail.com>
2024-11-06 13:07:25 -0500

    feat/adding erc1155 mint

--- components/ui/dialog/mintTokenDialog.tsx
+++ components/ui/dialog/mintTokenDialog.tsx
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import BaseDialog from '@/components/ui/dialog/baseDialog'
-import { MetamaskIcon, ErrorCircleIcon, CheckCircleIcon, CopyIcon, HelpCircleIcon } from '@/components/icons'
+import {
+  MetamaskIcon,
+  ErrorCircleIcon,
+  CheckCircleIcon,
+  CopyIcon,
+  HelpCircleIcon,
+} from '@/components/icons'
 import { copyToClipboard, formatAddress } from '@/lib/utils'
 import { Tooltip, TooltipContent, TooltipTrigger } from '../tooltip'
 import { useAuth } from '@/context/AuthContext'
@@ -17,38 +23,38 @@ type props = {
 }
 
 function MintTokenDialog({ closeDialog, open, address, strategy }: props) {
-  const { isLoggedIn, env } = useAuth();
-  const { mint, isError, setIsError, txHash, isMinted } = useMintToken();
-  const [isMinting, setIsMinting] = useState(false);
+  const { isLoggedIn, env } = useAuth()
+  const { mint, isError, setIsError, txHash, isMinted } = useMintToken()
+  const [isMinting, setIsMinting] = useState(false)
   const [isFormCompleted, setIsFormCompleted] = useState(false)
-  const [contractAddressCopied, setContractAddressCopied] = useState(false);
+  const [contractAddressCopied, setContractAddressCopied] = useState(false)
 
   useEffect(() => {
     if (contractAddressCopied) {
       const timer = setTimeout(() => {
-        setContractAddressCopied(false);
-      }, 1000);
-      return () => clearTimeout(timer);
+        setContractAddressCopied(false)
+      }, 1000)
+      return () => clearTimeout(timer)
     }
-  }, [contractAddressCopied]);
+  }, [contractAddressCopied])
 
   const [formData, setFormData] = useState({
-    addressTo: "",
-    amount: "",
-  });
+    addressTo: '',
+    amount: '',
+    tokenId: '0',
+  })
 
   const hasEmptyField = () => {
-    return Object.entries(formData).some(([key, value]) => value === "");
-  };
+    return Object.entries(formData).some(([key, value]) => value === '')
+  }
 
   const handleChange = (e: { target: { name: any; value: any } }) => {
-    const { name, value } = e.target;
+    const { name, value } = e.target
     setFormData((prevData) => ({
       ...prevData,
       [name]: value,
-    }));
-
-  };
+    }))
+  }
 
   useEffect(() => {
     setIsFormCompleted(!hasEmptyField())
@@ -56,14 +62,14 @@ function MintTokenDialog({ closeDialog, open, address, strategy }: props) {
 
   const handleSubmit = () => {
     setIsMinting(true)
-
     try {
       setTimeout(async () => {
         const params: MintTokenParams = {
           contractAddress: address,
           strategy: strategy,
           addressTo: formData.addressTo,
-          amount: formData.amount
+          amount: formData.amount,
+          tokenId: formData.tokenId,
         }
         await mint(params)
         setIsMinting(false)
@@ -76,23 +82,29 @@ function MintTokenDialog({ closeDialog, open, address, strategy }: props) {
   }
 
   return (
-
     <BaseDialog
       closeDialog={closeDialog}
       open={open}
-      className={(isMinting || isError || isMinted ? "h-[350px] w-[500px]" : "h-[500px] w-[600px]") + " text-white"}
+      className={
+        (isMinting || isError || isMinted
+          ? 'h-[350px] w-[500px]'
+          : strategy === DEPLOY_STRATEGY_ENUM.ERC1155
+          ? 'h-[600px] w-[600px]'
+          : 'h-[500px] w-[600px]') + ' text-white'
+      }
     >
       {isError || isMinted ? (
         <div className="flex flex-col items-center">
           <h2 className="text-2xl text-slate-100 text-center font-semibold mb-6 mt-8">
-            {isMinted ? "Token minted!" : "Ups! Something went wrong"}
+            {isMinted ? 'Token minted!' : 'Ups! Something went wrong'}
           </h2>
 
-          <>{isMinted ? (
-            <CheckCircleIcon className="w-[140px] h-[140px] text-custom-green" />
-          ) : (
-            <ErrorCircleIcon className="w-[140px] h-[140px]" />
-          )}
+          <>
+            {isMinted ? (
+              <CheckCircleIcon className="w-[140px] h-[140px] text-custom-green" />
+            ) : (
+              <ErrorCircleIcon className="w-[140px] h-[140px]" />
+            )}
           </>
           <a
             href={env.EXPLORER_TX_BASE_URL + txHash}
@@ -106,7 +118,7 @@ function MintTokenDialog({ closeDialog, open, address, strategy }: props) {
       ) : (
         <div>
           {isMinting ? (
-            <div className='flex flex-col justify-center items-center'>
+            <div className="flex flex-col justify-center items-center">
               <h2 className="text-2xl text-slate-100 text-center font-semibold mb-6 mt-8">
                 Minting token
               </h2>
@@ -140,11 +152,14 @@ function MintTokenDialog({ closeDialog, open, address, strategy }: props) {
                       <HelpCircleIcon className="w-4 h-4" />
                     </TooltipTrigger>
                     <TooltipContent>
-                      <p>This is the contract address from which you will mine your tokens. You can not edit this field. </p>
+                      <p>
+                        This is the contract address from which you will mine
+                        your tokens. You can not edit this field.{' '}
+                      </p>
                     </TooltipContent>
                   </Tooltip>
                 </div>
-                <div className='relative w-full text-zinc-400'>
+                <div className="relative w-full text-zinc-400">
                   <input
                     type="text"
                     name="contractAddress"
@@ -153,21 +168,21 @@ function MintTokenDialog({ closeDialog, open, address, strategy }: props) {
                     value={formatAddress(address)}
                     className="mt-2 w-full px-3 py-2 border border-[hsl(var(--border))] rounded-md bg-[hsl(var(--card))] cursor-not-allowed"
                   />
-                  <div className='absolute right-4 top-2 inset-y-0 flex items-center font-semibold'>
+                  <div className="absolute right-4 top-2 inset-y-0 flex items-center font-semibold">
                     <Tooltip>
                       <TooltipTrigger>
                         {contractAddressCopied ? (
                           <CheckCircleIcon
                             onClick={() => {
-                              copyToClipboard(address!);
+                              copyToClipboard(address!)
                             }}
                             className="w-5 h-5 text-green-500"
                           ></CheckCircleIcon>
                         ) : (
                           <CopyIcon
                             onClick={() => {
-                              copyToClipboard(address!);
-                              setContractAddressCopied(true);
+                              copyToClipboard(address!)
+                              setContractAddressCopied(true)
                             }}
                             className="w-5 h-5 hover:text-custom-pink cursor-pointer"
                           />
@@ -187,7 +202,9 @@ function MintTokenDialog({ closeDialog, open, address, strategy }: props) {
                       <HelpCircleIcon className="w-4 h-4" />
                     </TooltipTrigger>
                     <TooltipContent>
-                      <p>Enter the address where you want to send your tokens</p>
+                      <p>
+                        Enter the address where you want to send your tokens
+                      </p>
                     </TooltipContent>
                   </Tooltip>
                 </div>
@@ -200,6 +217,31 @@ function MintTokenDialog({ closeDialog, open, address, strategy }: props) {
                   className="mt-2 w-full px-3 py-2 border border-[hsl(var(--border))] rounded-md bg-[hsl(var(--card))] focus:border-gray-200 focus:outline-none"
                 />
               </div>
+              {strategy === DEPLOY_STRATEGY_ENUM.ERC1155 && (
+                <div className="flex flex-col w-full my-3 px-8">
+                  <div className="flex-row flex w-full flex gap-2 items-center">
+                    <label htmlFor="tokenId" className="block">
+                      ERC1155 Token ID
+                    </label>
+                    <Tooltip>
+                      <TooltipTrigger>
+                        <HelpCircleIcon className="w-4 h-4" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Enter the token ID from which you want to mint</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </div>
+                  <input
+                    type="text"
+                    name="tokenId"
+                    id="tokenId"
+                    value={formData.tokenId}
+                    onChange={handleChange}
+                    className="mt-2 w-full px-3 py-2 border border-[hsl(var(--border))] rounded-md bg-[hsl(var(--card))] focus:border-gray-200 focus:outline-none"
+                  />
+                </div>
+              )}
               <div className="flex flex-col w-full my-3 px-8">
                 <div className="flex-row flex gap-2 items-center">
                   <label htmlFor="amount" className="block">
@@ -223,7 +265,7 @@ function MintTokenDialog({ closeDialog, open, address, strategy }: props) {
                   className="mt-2 w-full px-3 py-2 border border-[hsl(var(--border))] rounded-md bg-[hsl(var(--card))] focus:border-gray-200 focus:outline-none"
                 />
               </div>
-              <div className='justify-end w-full flex items-center mt-6 mr-8 mb-4'>
+              <div className="justify-end w-full flex items-center mt-6 mr-8 mb-4">
                 {!isLoggedIn ? (
                   <ConnectWalletButton title="Connect wallet to mint" />
                 ) : (
