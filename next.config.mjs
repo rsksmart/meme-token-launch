5afb3d2c78d9c63ed38ca16a7560c68ff3546fb6
Turbobot <turbobot@vercel.com>
2025-06-07 10:57:39 -0500

    feat(create-turbo): apply official-starter transform

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-  }
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'ipfs.io',
+                pathname: '/ipfs/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'gateway.pinata.cloud',
+                pathname: '/ipfs/**',
+            }
+        ],
+    },
+}
   
-  export default nextConfig
+export default nextConfig
   
